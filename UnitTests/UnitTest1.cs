32a982e3db91340df349cc60b3d00c1b265474af
Hummel <chrisschulz@gmx.net>
2018-12-27 17:27:21 +0100

    switch to .net framwork

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace UnitTests
+{
+    
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
