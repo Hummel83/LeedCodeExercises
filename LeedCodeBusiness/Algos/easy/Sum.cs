6277cf2084703a34d288135f2c302a93a9d3083c
Hummel <chrisschulz@gmx.net>
2019-04-14 22:28:36 +0200

    delete white space

--- LeedCodeBusiness/Algos/easy/Sum.cs
+++ LeedCodeBusiness/Algos/easy/Sum.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-
-namespace LeedCodeBusiness.Algos.easy
+﻿namespace LeedCodeBusiness.Algos.easy
 {
     public class Sum
     {
@@ -9,15 +7,14 @@ namespace LeedCodeBusiness.Algos.easy
             int[] _sum = new int[2];
 
             for (int firstNumber = 0; firstNumber < nums.Length; firstNumber++)
-            {                
-                
+            {
                 for (int nextNumber = 0; nextNumber < nums.Length; nextNumber++)
                 {
                     if (target == nums[firstNumber] + nums[nextNumber])
                     {
                         _sum[1] = firstNumber;
                         _sum[0] = nextNumber;
-                        break;                        
+                        break;
                     }
                 }
             }
