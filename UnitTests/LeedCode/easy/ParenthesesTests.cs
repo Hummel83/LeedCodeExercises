64da8e6466f886d025e565dbb4531cae0548e03f
Christian Schulz <chrisschulz@gmx.net>
2019-12-23 12:45:06 +0100

    add tests

--- UnitTests/LeedCode/easy/ParenthesesTests.cs
+++ UnitTests/LeedCode/easy/ParenthesesTests.cs
@@ -5,18 +5,37 @@ namespace UnitTests.LeedCode.easy
 {
     public class ParenthesesTests
     {
-        [Fact]
-        public void Test123Valid()
+        [Theory]
+        [InlineData("(()())(())", "()()()")]
+        public void DeleteTwoOuterParentheses(string InputValue, string ExceptValue)
         {
-            //Arrange
-            var Input = "(()())(())";
-            var output = "()()()";
-
             //Act
-            var rop = new Parentheses().RemoveOuterParentheses(Input);
+            var rop = new Parentheses().RemoveOuterParentheses(InputValue);
 
             //Assert
-            Assert.Equal(rop, output);
+            Assert.Equal(ExceptValue,rop);
+        }
+
+        [Theory]
+        [InlineData("(()())(())(()(()))", "()()()()(())")]
+        public void DeleteFourOuterParentheses(string InputValue, string ExceptValue)
+        {
+            //Act
+            var rop = new Parentheses().RemoveOuterParentheses(InputValue);
+
+            //Assert
+            Assert.Equal(ExceptValue, rop);
+        }
+
+        [Theory]
+        [InlineData("()()", "")]
+        public void DeleteNothing(string InputValue, string ExceptValue)
+        {
+            //Act
+            var rop = new Parentheses().RemoveOuterParentheses(InputValue);
+
+            //Assert
+            Assert.Equal(ExceptValue, rop);
         }
     }
 }
