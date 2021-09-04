a832a919037412dff51301156877d7c215e7f77c
Christian Schulz <chrisschulz@gmx.net>
2021-09-04 20:58:10 +0200

    save progress

---
+++ LeedCodeBusiness/Algos/easy/LongestCommonPre.cs
@@ -0,0 +1,13 @@
+﻿namespace LeedCodeBusiness.Algos.easy
+{
+    // number 14
+    public class LongestCommonPre
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            var str = "";
+
+            return str;
+        }
+    }
+}
