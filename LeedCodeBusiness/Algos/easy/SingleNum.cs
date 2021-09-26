babfe8b816ba39a7684b394247f88fd0dd7c1949
Christian Schulz <chrisschulz@gmx.net>
2021-09-26 18:24:43 +0200

    add tests

---
+++ LeedCodeBusiness/Algos/easy/SingleNum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCodeBusiness.Algos.easy
+{
+    public class SingleNum
+    {
+
+
+
+        public int SingleNumber(int[] nums)
+        {
+
+            return 0;
+        }
+    }
+}
