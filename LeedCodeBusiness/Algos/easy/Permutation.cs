c21be288323823e912ee36ec756f5c0786755dd6
Christian Schulz <chrisschulz@gmx.net>
2021-11-19 21:14:38 +0100

    finish  1920

---
+++ LeedCodeBusiness/Algos/easy/Permutation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeedCodeBusiness.Algos.easy
+{
+    // nr 1920
+    public class Permutation
+    {
+        public int[] BuildArray(int[] nums)
+        {
+            int n = 1001;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] + (nums[nums[i]] % n) * n;
+              
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] / n;
+            }
+            return nums;
+        }
+    }
+}
---
+++ UnitTests/LeedCode/easy/PermutationTests.cs
@@ -0,0 +1,22 @@
+﻿using LeedCodeBusiness.Algos.easy;
+using Xunit;
+
+namespace UnitTests.LeedCode.easy
+{
+    public class PermutationTests
+    {
+        [Fact]
+        public void BuildArrayValid()
+        {
+            //Arrange
+            var input = new int[] { 0, 2, 1, 5, 3, 4 };
+            var output = new int[] { 0, 1, 2, 4, 5, 3 };
+
+            //Act
+            var result = new Permutation().BuildArray(input);
+
+            //Assert
+            Assert.Equal(result, output);
+        }
+    }
+}
